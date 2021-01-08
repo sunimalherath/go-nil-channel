37689f36a23ceee6d993be2857debf3a5b4a5ee1
sunimalherath <sunimaldv@gmail.com>
2021-01-08 22:56:30 +1100

    restarting channel

---
+++ nil-channel-start.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func reader(ch chan int) {
+	t := time.NewTimer(10 * time.Second)
+
+	for {
+		select {
+		case i := <-ch:
+			fmt.Printf("%d\n", i)
+		case <-t.C:
+			ch = nil
+		}
+	}
+}
+
+func writer(ch chan int) {
+	stopper := time.NewTimer(2 * time.Second)
+	starter := time.NewTimer(5 * time.Second)
+
+	savedCh := ch
+
+	for {
+		select {
+		case ch <- rand.Intn(42):
+		case <-stopper.C:
+			ch = nil
+		case <- starter.C:
+			ch = savedCh
+		}
+	}
+}
+func main() {
+	ch := make(chan int)
+
+	go reader(ch)
+	go writer(ch)
+
+	time.Sleep(20 * time.Second)
+}
